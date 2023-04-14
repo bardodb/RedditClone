bd01e62d841f8b7634517c691adaeff5c1e65e22
bardo_db <98047036+bardodb@users.noreply.github.com>
2023-04-14 16:41:28 -0300

    Add files via upload
    tive que upar tudo manualmente por erro no git no intelijj

---
+++ RedditBlog/src/main/java/com/portfolio/blog/redditblog/RedditBlogApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.blog.redditblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditBlogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RedditBlogApplication.class, args);
+  }
+
+}
---
+++ RedditBlog/src/test/java/com/portfolio/blog/redditblog/RedditBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.blog.redditblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditBlogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
